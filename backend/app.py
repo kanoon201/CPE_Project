15196d8cac5151f2d8b67267d1f3c701f5613aa5
Natdanai PHONGPENSRI <natdanaip129w@gmail.com>
2026-02-26 00:18:57 +0700

    Add register botton

--- backend/app.py
+++ backend/app.py
@@ -81,6 +81,12 @@ def login():
             return "Invalid username or password"
     return render_template("login.html")
 
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == 'POST': 
+        return redirect('/login')
+    return render_template('register.html')
+
 if __name__ == '__main__':
     try:
         init_mysql_db()
