9db5bd22abe9f1793863dd1ee058f0f654021f0d
Natdanai PHONGPENSRI <natdanaip129w@gmail.com>
2026-02-25 23:18:13 +0700

    Add login page

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 from db import get_mysql_connection, get_mongo_collection
 import mysql.connector
 
@@ -69,6 +69,17 @@ def init_mysql_db():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "admin" and password == "admin1234":
+            return redirect("/")
+        else:
+            return "Invalid username or password"
+    return render_template("login.html")
 
 if __name__ == '__main__':
     try:
